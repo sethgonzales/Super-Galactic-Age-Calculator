a980cacf9affb378716480b04eeda6cabaec5a05
sethgonzales <sethgonzales157@gmail.com>
2023-09-01 08:48:45 -0700

    add Test 1. Good Fail Test 1.

--- __tests__/javascript.test.js
+++ __tests__/javascript.test.js
@@ -1,4 +1,13 @@
-// import Constructor from './../src/javascript.js';
+import Person from './../src/person.js';
 
-// describe('Constructor', () => {}
-// test() => {}
+describe('Person', () => {
+  let sethPerson;
+
+  beforeEach(() => {
+    sethPerson = new Person("Seth", 26);
+  });
+  test('it should return the properties of the Person class instance of newPerson', () => {
+    expect(sethPerson).toEqual({ name: "Seth", age: "26" });
+  });
+
+});
