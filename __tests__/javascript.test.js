d863a9a32aa1e42225665e8e64984ebbaea0c27d
sethgonzales <sethgonzales157@gmail.com>
2023-09-01 09:29:07 -0700

    pass Test 2

--- __tests__/javascript.test.js
+++ __tests__/javascript.test.js
@@ -11,8 +11,8 @@ describe('Person', () => {
     expect(sethPerson).toEqual({ name: "Seth", earthAge: 26 });
   });
 
-  test('it should calculate sethPerson age in different planetary years and values to Person class', () => {
-    sethPerson.galacticAge(26);
+  test('it should calculate sethPerson age in different planetary years and values to Person class with one decimal place', () => {
+    sethPerson.galacticAge();
     expect(sethPerson).toEqual({ name: "Seth", earthAge: 26, mercuryAge: 108.3, venusAge: 41.9, marsAge: 13.8, jupiterAge: 2.2});
   });
 });
--- src/person.js
+++ src/person.js
@@ -4,7 +4,10 @@ export default class Person {
     this.earthAge = age;
   }
 
-  galacticAge(age) {
-
+  galacticAge() {
+    this.mercuryAge = parseFloat((this.earthAge * (1/0.24)).toFixed(1));
+    this.venusAge = parseFloat((this.earthAge * (1/0.62)).toFixed(1));
+    this.marsAge = parseFloat((this.earthAge * (1/1.88)).toFixed(1));
+    this.jupiterAge = parseFloat((this.earthAge * (1/11.86)).toFixed(1));
   }
 }
