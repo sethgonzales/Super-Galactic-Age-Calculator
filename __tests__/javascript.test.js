a73bcbd721dfa3aad315b59414bd1b1dec985135
sethgonzales <sethgonzales157@gmail.com>
2023-09-01 09:05:04 -0700

    add Test 2. Good fail Test 2.

--- __tests__/javascript.test.js
+++ __tests__/javascript.test.js
@@ -6,8 +6,12 @@ describe('Person', () => {
   beforeEach(() => {
     sethPerson = new Person("Seth", 26);
   });
-  test('it should return the properties of the Person class instance of newPerson', () => {
+
+  test('it should return the properties of the Person class instance of sethPerson', () => {
     expect(sethPerson).toEqual({ name: "Seth", earthAge: 26 });
   });
 
+  test('it should calculate sethPerson age in Mars years and add Mars age to Person class', () => {
+    expect(sethPerson.marsAge).toEqual(13.8);
+  });
 });
--- src/person.js
+++ src/person.js
@@ -3,4 +3,9 @@ export default class Person {
     this.name = name;
     this.earthAge = age;
   }
+
+  calcMarsAge(person) {
+
+  }
+
 }
