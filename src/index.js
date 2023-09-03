4d86f434c96ef944db4fd4a980152a091c32503c
sethgonzales <sethgonzales157@gmail.com>
2023-09-03 08:50:36 -0700

    connect the years since form to index.js

--- src/index.js
+++ src/index.js
@@ -3,28 +3,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Person from './person.js';
 
-function handleNameForm (event) {
+function handleNameForm(event) {
   event.preventDefault();
 
   const nameInput = document.querySelector('#name-input').value;
   const ageInput = document.querySelector('#age-input').value;
 
-  const newPerson = new Person (nameInput, ageInput);
+  const newPerson = new Person(nameInput, ageInput);
 
+  document.getElementById("response-container").removeAttribute("class");
 }
 
-function handleYearsUntilForm (event) {
+function handleYearsSinceForm(event) {
   event.preventDefault();
 
-  const nameInput = document.querySelector('#name-input').value;
-  const ageInput = document.querySelector('#age-input').value;
+  const planetSince = document.querySelector('input[name=planet-since]:checked').value;
+  const ageSince = document.querySelector('input#age-since').value;
+  const yearsSince = newPerson.galacticYearsSince(planetSince, ageSince);
 
+  document.getElementById("planet-years-since").innerText = yearsSince;
+  document.getElementById("planet-since").innerText = planetSince;
+  document.getElementById("years-since").innerText = yearsSince;
+
+  document.getElementById("years-since-resp").removeAttribute("class");
 
 }
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   document.querySelector("#name-age-form").addEventListener("submit", handleNameForm);
   document.querySelector("#years-since-form").addEventListener("submit", handleYearsSinceForm);
-  document.querySelector("#years-until-form").addEventListener("submit", handleYearsUntilForm);
+  // document.querySelector("#years-until-form").addEventListener("submit", handleYearsUntilForm);
 
 });
